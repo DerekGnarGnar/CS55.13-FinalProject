a6ade618c13af93610a15a435c5095de50352859
Derek Nielsen <dnielsen85@gmail.com>
2022-09-14 23:34:00 +0000

    initial repo commit

---
+++ components/layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+
+
+export default function Layout({children, home}){
+  return (
+
+    <div>
+      <Head>
+        <title>Basic Next.js App</title>
+      </Head>
+      <main>{children}</main>
+      {!home && (
+      <Link href="/">
+        <a className="btn btn-primary mt-3">←	Back to Home</a>
+      </Link>
+      )}
+    </div>
+  );
+}
---
+++ pages/api/get.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs';
+// path
+import path from 'path';
+//use path to build a filepath to our data subdirectory
+const dataDir = path.join(process.cwd(),"data");
+console.log(dataDir);
+
+export default function handler(req, res) {
+  const filePath = path.join(dataDir, "persons.json");
+  const jsonData = fs.readFileSync(filePath, "utf8");
+  const jsonObj = JSON.parse(jsonData);
+
+  jsonObj.sort(
+    function(a,b){
+      return a.name.localeCompare(b.name);
+    }
+  );
+  
+  res.status(200).json(jsonObj);
+  
+}
